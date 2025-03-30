e6d8ed00e77694580cb68e6c18b7a6369c6f0a0d
tranphucc <154044911+tranphucc@users.noreply.github.com>
2025-03-30 19:39:38 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password', 'role', 'birth', 'gender', 'address',
+        'name', 'email', 'password', 
+        'role', 'birth', 'gender', 'address'
     ];
 
     protected $hidden = [
-        'password',
-    ];
-    protected $casts = [
-        'birth' => 'date', // Tự động chuyển đổi ngày sinh thành đối tượng Carbon
+        'password', 'remember_token'
     ];
 
+    protected $casts = [
+        'birth' => 'date',
+        'email_verified_at' => 'datetime'
+    ];
+
+    // Kiểm tra role admin
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
 }
