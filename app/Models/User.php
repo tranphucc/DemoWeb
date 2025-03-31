ddaa1773d11a6ec71b41b70049a860616b40ef9f
thephuc <phuc02500@gmail.com>
2025-03-31 13:33:31 +0700

    thephuc

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,20 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password', 'role', 'birth', 'gender', 'address',
+        'name', 
+        'email', 
+        'password', 
+        'role', 
+        'birth', 
+        'gender', 
+        'address',
     ];
 
     protected $hidden = [
         'password',
-    ];
-    protected $casts = [
-        'birth' => 'date', // Tự động chuyển đổi ngày sinh thành đối tượng Carbon
+        'remember_token', // Thêm vào để hỗ trợ remember me
     ];
 
+    protected $casts = [
+        'birth' => 'date',
+        'email_verified_at' => 'datetime', // Nếu có xác thực email
+    ];
+
+    // Tự động hash password khi set
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::needsRehash($value) 
+            ? Hash::make($value) 
+            : $value;
+    }
 }
